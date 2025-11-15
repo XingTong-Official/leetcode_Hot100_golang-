b26cf844d55bcd571c1470f95c9a743fea785f53
XingTong-Official <2896309070@qq.com>
2025-11-15 22:59:12 +0800

    2025/11/15 evening

---
+++ 94. 二叉树的中序遍历/main.go
@@ -0,0 +1,73 @@
+package main
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type stack struct {
+	arrs   []*TreeNode
+	length int
+}
+
+func NewStack() *stack {
+	return &stack{arrs: make([]*TreeNode, 0, 40), length: 0}
+}
+func (s *stack) Pop() *TreeNode {
+	value := s.Top()
+	s.arrs = s.arrs[:s.length-1]
+	s.length--
+	return value
+}
+func (s *stack) Top() *TreeNode {
+	return s.arrs[s.length-1]
+}
+func (s *stack) Push(node *TreeNode) {
+	s.arrs = append(s.arrs, node)
+	s.length++
+}
+
+// 迭代
+func inorderTraversal(root *TreeNode) []int {
+	stack := NewStack()
+	answer := []int{}
+	for root != nil || stack.length != 0 {
+		for root != nil {
+			stack.Push(root)
+			root = root.Left
+		}
+		root = stack.Pop()
+		answer = append(answer, root.Val)
+		root = root.Right
+	}
+	return answer
+}
+
+//闭包的递归
+// func inorderTraversal(root *TreeNode) []int {
+// 	answer := []int{}
+// 	var traverse func(*TreeNode)
+// 	traverse = func(node *TreeNode) {
+// 		if node == nil {
+// 			return
+// 		}
+// 		traverse(node.Left)
+// 		answer = append(answer, node.Val)
+// 		traverse(node.Right)
+// 	}
+// 	traverse(root)
+// 	return answer
+// }
